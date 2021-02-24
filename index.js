8548b703aa5b5bd6abaf00fab2e4acac6b0a0be6
sarah-maau <maurysarah@hotmail.fr>
2021-02-24 08:47:50 +0100

    fix #12 mise en place de CORS

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3300;
+const cors = require('cors');
+
+// configuration de Swagger 
+// visible depuis localhost:port/api-docs
 const expressSwagger = require('express-swagger-generator')(app);
 
 let options = require('./config-swagger.json');
+
 options.basedir = __dirname;
+
 options.swaggerDefinition.host = `localhost:${port}`;
 
 expressSwagger(options)
 
 const router = require('./app/router');
+
+// autorisation : tout le monde peut venir chercher l'info
+app.use(cors());
 
 app.use(express.json());
 
